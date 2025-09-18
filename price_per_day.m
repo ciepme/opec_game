fa45511a05bfeb7b3077d341723a2d84286e92c0
Jacob Cieply <114676624+ciepme@users.noreply.github.com>
2025-09-18 17:36:19 -0400

    Finite Max

--- price_per_day.m
+++ price_per_day.m
@@ -1,3 +1,3 @@
 function price = price_per_day(P, opec_production, demand_shock, supply_shock)
-    price = P(sum(opec_production),demand_shock, supply_shock);
+    price = P(opec_production, demand_shock, supply_shock);
 end
