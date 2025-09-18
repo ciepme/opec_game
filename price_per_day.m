16b903d27d53d62535f7de4abd3632405874f07c
Jacob Cieply <114676624+ciepme@users.noreply.github.com>
2025-09-18 13:58:40 -0400

    Nigeria Hegemony

--- price_per_day.m
+++ price_per_day.m
@@ -1,3 +1,3 @@
-function price = price_per_day(P, opec_production)
-    price = P(sum(opec_production));
+function price = price_per_day(P, opec_production, demand_shock, supply_shock)
+    price = P(sum(opec_production),demand_shock, supply_shock);
 end
